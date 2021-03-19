f60f14c781e4d447eba943e34521ab56ec9c860d
sbeoarng <seanborg8@gmail.com>
2021-03-19 19:52:29 +0000

    setting up seed scene

---
+++ ConferenceOfTheBirds/Assets/Scripts/Lever/LeverScript.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverScript : MonoBehaviour
+{
+    //Variables
+    private bool canToggleLever = false;
+    public bool isActive = false;
+
+    //GameObjects
+    public GameObject waterPipe;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canToggleLever)
+            ToggleLever();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            canToggleLever = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+            canToggleLever = false;
+    }
+
+    private void ToggleLever()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isActive = !isActive;
+        }
+    }
+}
---
+++ ConferenceOfTheBirds/Assets/Scripts/Lever/WaterPipe.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterPipe : MonoBehaviour
+{
+    //Variables
+    [HideInInspector]
+    public bool isPipeActive;
+        
+    //GameObjects
+    public GameObject pipeWaterPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
