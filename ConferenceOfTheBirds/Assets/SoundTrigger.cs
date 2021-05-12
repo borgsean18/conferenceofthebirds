267e3107f659fc184d694a633b20fe9e10b34688
Michelangelo <michelangelo.bedetti@gmail.com>
2021-05-12 11:13:57 +0100

    VO experiment

---
+++ ConferenceOfTheBirds/Assets/SoundTrigger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundTrigger : MonoBehaviour
+{
+
+    public AudioSource playSound;
+    
+    void OnTriggerEnter (Collider other)
+    {
+        playSound.Play();
+    }
+
+}
